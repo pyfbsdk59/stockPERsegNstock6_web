358d980421acd139faf8bd03e35c32659fc31fb3
pyfbsdk59 <pyfbsdk59@gmail.com>
2026-02-15 23:10:57 +0800

    3rd

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from stock_app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
