45a639750d5872a99e02d123102af509a2dc2c9f
pyfbsdk59 <pyfbsdk59@gmail.com>
2026-02-15 23:23:21 +0800

    5th

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url # 引入這個套件
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
