4d8473820f3c508a6f2c088fb44d1cec4cea9eb5
pyfbsdk59 <pyfbsdk59@gmail.com>
2026-02-16 08:38:52 +0800

    12th

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # 更新資料庫結構
-#python manage.py makemigrations
-#python manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 #清除資料庫資料
-python manage.py flush --no-input
-python manage.py migrate stock_app zero
-python manage.py migrate
+#python manage.py flush --no-input
+#python manage.py migrate stock_app zero
+#python manage.py migrate
